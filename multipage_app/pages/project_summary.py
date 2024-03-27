1c1cab23348791c0fbb0969b84a31f8b2a61d094
Jamie Roche <jamieroche1987@hotmail.co.uk>
2024-03-27 21:39:19 +0000

    Update homepage.py and project summary.py

--- multipage_app/pages/project_summary.py
+++ multipage_app/pages/project_summary.py
@@ -1,18 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.write("## Project Summary")
 
 st.info(
-    f"Test1"
-)
-
-
-st.write(
-    f"Test2"
-)
-
-st.success(
-    f"Test3"
-)
-
-st.sidebar.write("Select page above to view")
+    f"**Background information:**\n\n"
+    f"As a good friend, you are requested by your friend, who has received\n"
+    f"an inheritance from a deceased great-grandfather located in\n"
+    f"Ames,Iowa, to help in maximising the sales price for\n"
+    f"the inherited properties.\n\n"
+    f"Although your friend has an excellent understanding of property prices\n"
+    f"in her own state and residential area, she fears that basing her\n"
+    f"estimates for property worth on her current knowledge might lead to\n"
+    f"inaccurate appraisals.\n"
